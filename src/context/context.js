55844b28d77bd35f7f8fa0501afae0fcdf3d1307
shadmanAh <shadman061@gmail.com>
2023-08-31 11:21:38 +0330

    get repos and followers from github

--- src/context/context.js
+++ src/context/context.js
@@ -14,21 +14,33 @@ const GithubProvider = ({ children }) => {
   const [followers, setFollowers] = useState(mockFollowers)
 
   const [requests, setRequests] = useState(0)
-  const [loading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState({ show: false, msg: '' })
 
   const searchGithubUser = async (user) => {
     toggleError()
+    setIsLoading(true)
 
     const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
       console.log(err)
     )
-    console.log(response)
+
     if (response) {
       setGithubUser(response.data)
+      const { login, followers_url } = response.data
+      //repos
+      axios(`${rootUrl}/users/${login}/repos?per_page=100`).then((response) =>
+        setRepos(response.data)
+      )
+      //followers
+      axios(`${followers_url}?per_page=100`).then((response) =>
+        setFollowers(response.data)
+      )
     } else {
       toggleError(true, 'there is no user with that username!')
     }
+    checkRequests()
+    setIsLoading(false)
   }
 
   const checkRequests = () => {
@@ -60,6 +72,7 @@ const GithubProvider = ({ children }) => {
         requests,
         error,
         searchGithubUser,
+        isLoading,
       }}
     >
       {children}
